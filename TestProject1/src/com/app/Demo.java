c2d929f1d9b0dc8c4e9b36b15dea02f36d3660c4
testmbt <marc.bazil@gmail.com>
2017-05-23 13:34:31 -0500

    Added more comments

--- TestProject1/src/com/app/Demo.java
+++ TestProject1/src/com/app/Demo.java
@@ -4,8 +4,9 @@ public class Demo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+    System.out.println("New1");
+    System.out.println("New2");
     System.out.println("New3");
-    System.out.println("New41");
 	}
 
 }
